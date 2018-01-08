3b80489f4857ea3a687d2cfa15f6121fac575bcb
Jeff Bailey <northwestjeff@gmail.com>
2018-01-08 11:20:09 -0800

    working on model forms

--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class Tool(models.Model):
--- library/views.py
+++ library/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+
 from library.models import User, Tool
 
 
@@ -6,4 +10,17 @@ from library.models import User, Tool
 def home(request):
     tools = Tool.objects.all()
     return render(request, 'library/home.html', {"tools": tools})
+
+
+    def post(self, request):
+
+        if form.is_valid():
+            text = form.cleaned_data['post']
+
+        args
+
+        return render(request, 'library/home.html', {"tools": tools})
+
+
+
 
